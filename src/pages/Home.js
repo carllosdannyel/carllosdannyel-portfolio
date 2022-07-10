512d151a6bad9e1d3b2138766305b621d42cfc42
Carlos Daniel <carlosdanielpereirasantos18@gmail.com>
2022-07-09 23:04:11 -0300

    adicionando estilizacoes com tailwind no perfil

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,18 @@
+import { Link } from "react-router-dom";
+import Perfil from "../components/Perfil";
+
 const Home = () => {
-  return <h1 className="">Pagina Home</h1>;
+  return (
+    <div className="p-10">
+      <Perfil />
+      <Link
+        className="block shadow bg-green-800 text-white text-center p-4 mt-5 rounded-xl hover:bg-green-600"
+        to="/portfolio"
+      >
+        Acessar Portfólio
+      </Link>
+    </div>
+  );
 };
 
 export default Home;
